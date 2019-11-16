a1efc5838323126508796d5c3e20edc5c65ff7e7
Kamil Stasiak <kamilstasiaknew@gmail.com>
2019-11-16 20:42:48 +0100

    Add search result as card with image

--- src/universal_search_page/core.cljs
+++ src/universal_search_page/core.cljs
@@ -10,11 +10,40 @@
 
 (defonce app-state (atom {:text "Search"}))
 
+(def bulbasaur
+  {:name        "Bulbasaur"
+   :types       "Grass/Poison"
+   :image       "https://cdn.bulbagarden.net/upload/2/21/001Bulbasaur.png"
+   :description "Bulbasaur is a dual-type Grass/Poison Pokémon introduced in Generation I.\n\nIt evolves into Ivysaur starting at level 16, which evolves into Venusaur starting at level 32.\n\nAlong with Charmander and Squirtle, Bulbasaur is one of three starter Pokémon of Kanto available at the beginning of Pokémon Red, Green, Blue, FireRed, and LeafGreen."})
+
+(defn search-bar []
+  [:div.search-bar
+   [:> mui/Input {:color "primary" :full-width true}]
+   [:> mui/Fab {:color "secondary"}
+    [:> icons/Send]]])
+
+(defn search-result [props]
+  [:> mui/Card {:class-name "search-result"}
+   [:> mui/CardHeader {:title (get-in props [:pokemon :name]) :subheader (get-in props [:pokemon :name])}]
+   [:> mui/CardMedia {:image      (get-in props [:pokemon :image])
+                      :title      (get-in props [:pokemon :name])
+                      :class-name "media"}]
+   [:> mui/CardContent
+    [:> mui/Typography (get-in props [:pokemon :description])]]
+   [:> mui/CardActions]
+   ])
+
+(defn search-results []
+  [:div.search-results
+   [search-result {:pokemon bulbasaur}]
+   [search-result {:pokemon bulbasaur}]
+   [search-result {:pokemon bulbasaur}]
+   ])
+
 (defn universal-search-page []
   [:div.app
-   [:div.search-bar
-    [:> mui/Input {:color "primary" :full-width true}]
-    [:> mui/Fab {:color "secondary"}
-     [:> icons/Send]]]])
+   [search-bar]
+   [search-results]
+   ])
 
 (reagent/render-component [universal-search-page] (. js/document (getElementById "app")))
