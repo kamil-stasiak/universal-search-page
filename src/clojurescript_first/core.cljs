3d3d887d77cc6da3342e7936b04f39eb8f794a36
Kamil Stasiak <kamilstasiaknew@gmail.com>
2019-11-16 18:47:24 +0100

    Add grid to search

--- src/clojurescript_first/core.cljs
+++ src/clojurescript_first/core.cljs
@@ -1,8 +1,9 @@
 (ns clojurescript-first.core
-  (:require [reagent.core :as reagent :refer [atom]]
+  (:require [reagent.core :as reagent :refer [atom, reactify-component]]
             [material-ui :as mui]
             [material-ui-icons :as icons]
             [goog.object :as gobj]
+            ["@material-ui/core/styles" :refer [createMuiTheme withStyles]]
             ["material-ui/colors" :as mui-colors]))
 
 (enable-console-print!)
@@ -10,11 +11,10 @@
 (defonce app-state (atom {:text "Search"}))
 
 (defn hello-world []
-  [:> mui/Container {:max-width "lg"}
-   [:> mui/Grid {:container true :direction "row" :justify "space-between" :align-items "flex-end"}
-    [:> mui/Grid {:item true :xs 11}
-     [:> mui/Input {:color "primary" :full-width true}]]
-    [:> mui/Grid {:item true :xs 1}
-     [:> mui/Fab {:color "secondary"} [:> icons/Send]]]]])
+  [:div.app
+   [:div.search-bar
+    [:> mui/Input {:color "primary" :full-width true}]
+    [:> mui/Fab {:color "secondary"}
+     [:> icons/Send]]]])
 
 (reagent/render-component [hello-world] (. js/document (getElementById "app")))
